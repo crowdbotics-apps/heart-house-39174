0d7245739cbf252534de90823ca7caffafa13ee5
Crowdbotics <team@crowdbotics.com>
2023-03-08 20:17:58 +0000

    Add Blank Screen 18081

---
+++ screens/BlankScreen18081/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen18081 = () => {
+  return <View style={_styles.BlankScreen18081}>
+      </View>;
+};
+
+export default BlankScreen18081;
+
+const _styles = StyleSheet.create({
+  BlankScreen18081: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
